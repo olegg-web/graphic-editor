09f214b95a2d208e492bf5b8e2d70982f31611c9
oleg <guljaev_oleg@mail.ru>
2020-02-21 14:19:47 +0300

    v1.4

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 window.addEventListener("load", function onWindowLoad() {
 	let canvas = document.querySelector('#canvas');
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
-	let context = canvas.getContext('2d');
-	context.lineCap = "round";
+	let context = canvas.getContext('2d');	
 	let boxPoint = document.querySelector('#boxPoint');
 	let range = document.querySelector('#range');
 	let coordinates = document.querySelector('#coordinates');
 	let myColor;
+	let drawLineMode, drawLine;
+	let lx, ly, ldx, ldy;
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	context.lineCap = "round";
 	generatePalette(document.querySelector("#palette"));
 
 
@@ -18,8 +20,33 @@ window.addEventListener("load", function onWindowLoad() {
 
 	document.querySelector(".eraser").onclick = function setWhiteColor() {
 		myColor = 'white';
-		
 	}
+
+	document.querySelector(".line").onclick = function drawLine() {
+		drawLineMode = true;
+		drawLine = true;
+	}
+
+	canvas.addEventListener('click', function drawLines(e) {
+		if (drawLineMode) {
+			if (drawLine) {
+				lx = e.offsetX;
+				ly = e.offsetY;
+				drawLine = false;
+			} else {
+				ldx = e.offsetX;
+				ldy = e.offsetY;
+				context.beginPath();
+				context.moveTo(lx, ly);
+				context.lineTo(ldx, ldy);
+				context.strokeStyle = myColor;
+				context.stroke();
+				context.closePath();
+				drawLine = true;
+			}
+		}
+	});
+
 
 	// выполняется при движение мыши 
 	canvas.onmousemove = function mouseMove(e) {
@@ -30,8 +57,8 @@ window.addEventListener("load", function onWindowLoad() {
 
 		coordinates.innerHTML = "X : " + x + " px " + "&nbsp;" + " Y : " + y + " px";
 
-		// Проверяем зажата ли какая-нибудь кнопка мыши
-		if (e.buttons > 0) {
+		// Проверяем зажата кнопка мыши
+		if (e.which == 1) {
 			context.beginPath();
 			context.moveTo(x, y);
 			context.lineTo(x - dx, y - dy);
@@ -46,20 +73,8 @@ window.addEventListener("load", function onWindowLoad() {
 		context.lineWidth = e.target.value;
 	}
 
-	// document.querySelector('#buttonClear').onclick = clear();
-
-	// document.keydown = function (e) {
-	// 	if (e.keyCode == 32) {
-	// 		clear();
-	// 	}
-	// }
-
-	// function clear() {
-	// 	context.clearRect(0, 0, canvas.width, canvas.height);
-	// }
-
-	document.querySelector("#buttonClear").onclick = function clear() {
-		context.clearRect(0, 0, canvas.width, canvas.height);
+	document.querySelector("#buttonClear").onclick = function clearBtn() {
+		clear();
 	};
 
 	function clear() {
@@ -73,21 +88,22 @@ window.addEventListener("load", function onWindowLoad() {
 	})
 
 	//сохранение
-	// document.getElementById("buttonSave").onclick = function save(){
-	download_img = function (e) {
-		var image = canvas.toDataURL("image/jpg");
-		e.href = image;
 
+	download_img = function (e) {
+		let image = canvas.toDataURL("image/jpg");
+		e.href = image;
 	}
+	//генерация цветов
 
 	function generatePalette(palette) {
-		for (var r = 0, max = 2; r <= max; r++) {
-			for (var g = 0; g <= max; g++) {
-				for (var b = 0; b <= max; b++) {
-					var paletteBlock = document.createElement('div');
+		for (let r = 0, max = 2; r <= max; r++) {
+			for (let g = 0; g <= max; g++) {
+				for (let b = 0; b <= max; b++) {
+					let paletteBlock = document.createElement('div');
 					paletteBlock.className = 'paletteBox';
 					paletteBlock.addEventListener('click', function changeColor(e) {
 						myColor = e.target.style.backgroundColor;
+						drawLineMode = false;
 					});
 
 					paletteBlock.style.backgroundColor = (
@@ -102,10 +118,3 @@ window.addEventListener("load", function onWindowLoad() {
 		}
 	}
 });
-
-
-
-
-
-
-
